aa0a37a4cfa3a3c947c3992dcf4cfbcd29ef9eb3
mohamedibrahimabdulghani <mohamedibrahimabdulghani@gmail.com>
2025-08-19 08:26:11 +0300

    make route web to add post

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Posts;
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Posts::get();
+        return view('posts.posts', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'body' => 'required|string',
+        ]);
+
+        Posts::create([
+            'title' => $request->title,
+            'body' => $request->body,
+        ]);
+
+        return redirect()->route('posts.index')->with('success', 'Post created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Posts $posts)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Posts $posts)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Posts $posts)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Posts $posts)
+    {
+        //
+    }
+}
