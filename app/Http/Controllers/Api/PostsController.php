eabc89ec7238aa3327fdf0b9fa1fd00dda0bd131
mohamedibrahimabdulghani <mohamedibrahimabdulghani@gmail.com>
2025-08-20 10:52:36 +0300

    format api response with data, and made trait file and use it

---
+++ app/Http/Controllers/Api/PostsController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Posts;
+use Illuminate\Http\Request;
+
+class PostsController extends Controller
+{
+    use apiResponseTrait;
+
+    public function index() {
+        $posts = Posts::all();
+        return $this->apiResponse($posts, 'Success, Get All Posts', 200); // this function called by apiResponseTrait File
+    }
+
+    public function show($id) {
+        $post = Posts::findOrFail($id);
+        return $this->apiResponse($post, 'Success Get Post By Id', 200); // this function called by apiResponseTrait File
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\PostsController as ApiPostsController;
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test', function () {
-    return ['message' => 'API is working!'];
-});
+// Route::get('/test', function () {
+//     return ['message' => 'API is working!'];
+// });
+
+
+Route::get('posts', [ApiPostsController::class, 'index'])->name('api.posts'); // this route to get all posts
+Route::get('post/{id}', [ApiPostsController::class, 'show'])->name('api.getPostById'); // this route to get a post by id
